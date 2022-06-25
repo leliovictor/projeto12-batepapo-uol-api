033b496b18b95a205f6f2ed1305ddbb87f407d19
leliovictor <leliov.vieira@gmail.com>
2022-06-25 14:42:18 -0300

    Added validation with joi in post/messages

--- server.js
+++ server.js
@@ -18,23 +18,19 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-const userSchema = joi.object({
-  name: joi.string().required(),
-});
+const userSchema = joi.string().required();
 
-//mudar para message Schema
 const messageSchema = joi.object({
-  from: joi.string().required(),
-  to: joi.number().required(),
-  text: joi.string().email().required(),
-  type: joi.string().email().required(),
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
 });
 
 server.post("/participants", async (req, res) => {
-  const userValidation = userSchema.validate(req.body);
-  if (userValidation.error) return res.sendStatus(422);
-
   const { name } = req.body;
+
+  const userValidation = userSchema.validate(name);
+  if (userValidation.error) return res.sendStatus(422);
 
   try {
     const checkAlreadyOn = await db.collection("users").findOne({ name: name });
@@ -54,7 +50,6 @@ server.post("/participants", async (req, res) => {
     });
 
     res.sendStatus(201);
-    
   } catch {
     res.sendStatus(500);
   }
@@ -69,19 +64,30 @@ server.get("/participants", async (req, res) => {
   }
 });
 
-server.post("/messages", (req, res) => {
-  const { to, text, type } = req.body;
+server.post("/messages", async (req, res) => {
   const { user } = req.headers;
+  const { to, text, type } = req.body;
 
-  db.collection("messages").insertOne({
-    from: user,
-    to,
-    text,
-    type,
-    time: dayjs().format("HH:mm:ss"),
-  });
+  try {
+    const checkParticipantOn = await db
+      .collection("users")
+      .findOne({ name: user });
+    const messageValidation = messageSchema.validate(req.body);
+    if (!checkParticipantOn || messageValidation.error)
+      return res.sendStatus(422);
 
-  res.sendStatus(201);
+    db.collection("messages").insertOne({
+      from: user,
+      to,
+      text,
+      type,
+      time: dayjs().format("HH:mm:ss"),
+    });
+
+    res.sendStatus(201);
+  } catch {
+    res.sendStatus(500);
+  }
 });
 
 server.get("/messages", async (req, res) => {
